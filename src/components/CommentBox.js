415cc5149d6fab20d0615afc288066ad8b22c9db
Cody Miller <cody.miller@learningfuze.com>
2018-10-16 17:56:10 -0700

    Initial Commit

---
+++ src/components/CommentBox.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class CommentBox extends Component {
+
+  render() {
+    return (
+      <div>
+        <h4>Add a Comment</h4>
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
---
+++ src/components/CommentList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class CommentList extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h4>Comment List</h4>
+      </div>
+    );
+  }
+}
+
+export default CommentList;
